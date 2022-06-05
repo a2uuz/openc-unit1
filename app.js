3f55306798a453d796821d0c9a3ff42289d08859
a2uuz <aziznageeye9@gmail.com>
2022-06-05 21:25:38 +0300

    js 01

---
+++ app.js
@@ -0,0 +1,32 @@
+// Create a class here
+// =====================================
+class Episode {
+  constructor(title, duration, hasBeenWatched) {
+    this.title = title;
+    this.duration = duration;
+    this.hasBeenWatched = hasBeenWatched;
+  }
+}
+
+let firstEpisode = new Episode("Love", 40, true);
+let secondEpisode = new Episode("Death", 40, false);
+let thirdEpisode = new Episode("Robot", 40, false);
+// =====================================
+
+document.querySelector("#first-episode-info").innerText = `Episode: ${
+  firstEpisode.title
+}
+Duration: ${firstEpisode.duration} min
+${firstEpisode.hasBeenWatched ? "Already watched" : "Not yet watched"}`;
+
+document.querySelector("#second-episode-info").innerText = `Episode: ${
+  secondEpisode.title
+}
+Duration: ${secondEpisode.duration} min
+${secondEpisode.hasBeenWatched ? "Already watched" : "Not yet watched"}`;
+
+document.querySelector("#third-episode-info").innerText = `Episode: ${
+  thirdEpisode.title
+}
+Duration: ${thirdEpisode.duration} min
+${thirdEpisode.hasBeenWatched ? "Already watched" : "Not yet watched"}`;
